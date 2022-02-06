097fa2cfb5c2d735562c791331d33e8403c45f3c
Mannyg448 <manny.g448@hotmail.com>
2022-02-06 17:25:20 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsk,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
