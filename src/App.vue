6f84bc80cd35d3ce8426e222b477d1bd4bbb49b5
Mannyg448 <manny.g448@hotmail.com>
2022-02-06 18:48:21 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -32,35 +32,35 @@ const todos = ref([]);
 </script>
 
 <template>
-<div class="min-h-screen bg-gray-800">
+<div class="min-h-screen bg-yellow-400">
   <div class="container flex flex-col pt-8 mx-auto space-y-8">
-  <h1 class="pb-4 text-6xl font-thin tracking-tight text-center text-gray-300">My Todo App</h1>
+  <h1 class="pb-4 text-6xl font-black tracking-tighter text-center text-black-300 ">My Todo App</h1>
   <input
   @change ="addTodo"
   v-model="newTodo" 
-  type="text" class="rounded-lg text-center px=4 py-2 text-xl" placeholder="New Todo"/>
+  type="text" class="rounded-lg text-center px=4 py-2 text-xl text-white bg-black" placeholder="Add a new todo function."/>
   <div class="flex justify-around">
   <div class="w-1/3 space-y-4">
-    <h3 class="text-2xl text-center text-blue-400 ">Pending</h3>
+    <h3 class="text-2xl font-bold text-center text-black ">Pending</h3>
     <ul class="pt-8 space-y-4">
       <li v-for="todo in pending"
       :key="todo.id"
       @click="changeStatus(todo.id)"
-      class="w-full px-4 py-2 font-bold text-center text-blue-600 duration-500 bg-gray-300 rounded-lg hover:cursor-pointer hover:bg-blue-400 hover:text-gray-200 trasition-colors">
+      class="w-full px-4 py-2 font-bold text-center text-yellow-400 duration-500 bg-black border-2 border-black rounded-lg hover:cursor-pointer hover:bg-yellow-400 hover:text-black trasition-colors">
       {{todo.content}}
       </li>
       
     </ul>
     </div>
     <div class="w-1/3 space-y-4">
-        <h3 class="text-2xl text-center text-green-600 ">Completed</h3>
+        <h3 class="text-2xl font-bold text-center text-green-600 ">Completed</h3>
 
     <ul class="pt-8 space-y-4">
       <li 
       v-for="todo in completed"
       :key="todo.id"
       @click="changeStatus(todo.id)"
-      class="w-full px-4 py-2 font-bold text-center text-green-600 duration-500 bg-gray-300 rounded-lg hover:cursor-pointer hover:bg-green-400 hover:text-gray-200 trasition-colors"> {{todo.content}}</li>
+      class="w-full px-4 py-2 font-bold text-center text-white duration-500 bg-green-600 rounded-lg hover:cursor-pointer border-green hover:bg-white hover:text-green-600 trasition-colors"> {{todo.content}}</li>
     </ul>
   </div>
   </div>
